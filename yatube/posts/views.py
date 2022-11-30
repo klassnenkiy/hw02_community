132424e4c8269120f50e4d3d356729f899a62470
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2022-12-01 02:01:12 +0300

    2nd

---
+++ yatube/posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+from yatube.settings import COUNT_POSTS
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:COUNT_POSTS]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:COUNT_POSTS]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
