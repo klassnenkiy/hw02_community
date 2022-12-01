c8a6699df40560a34bdf807a914aca94b5f16dc0
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2022-12-01 23:44:29 +0300

    3rd

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,7 @@ from yatube.settings import COUNT_POSTS
 
 
 def index(request):
-    posts = Post.objects.all()[:COUNT_POSTS]
+    posts = Post.objects.select_related('author', 'group').all()[:COUNT_POSTS]
     context = {
         'posts': posts,
     }
@@ -13,7 +13,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:COUNT_POSTS]
+    posts = group.posts.select_related('author')[:COUNT_POSTS]
     context = {
         'group': group,
         'posts': posts,
