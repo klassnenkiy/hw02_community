9b720c6ce9395b91787372f9ad5e6c58ab561496
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2022-12-01 23:48:00 +0300

    3rd

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')[:COUNT_POSTS]
+    posts = group.posts.select_related('author', 'group')[:COUNT_POSTS]
     context = {
         'group': group,
         'posts': posts,
